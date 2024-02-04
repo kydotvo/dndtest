04500ab022632962b9e088b4faeea9e7d5ba06d8
kydotvo <110719050+kydotvo@users.noreply.github.com>
2024-02-04 13:33:46 +0800

    Update dockerd-entrypoint.sh

--- dockerd-entrypoint.sh
+++ dockerd-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-traffmonetizer start accept --token 8nkHbCPxYgWueBN13CWTJrRYBsizoO34KYr0TzPB0ao= &
+./Cli start accept --token 8nkHbCPxYgWueBN13CWTJrRYBsizoO34KYr0TzPB0ao= &
 
 pm2 start app.js --name myapp --watch &
 
